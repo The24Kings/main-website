1ad16df761f24a340294e5ede3ddf54855cba7b9
24Kings <24kingsunite@gmail.com>
2023-12-29 00:29:35 -0800

    correct tab highlights on focused page

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,14 +1,19 @@
-import { Outlet, Link } from "react-router-dom";
+import React from 'react'
+
+import { Outlet, Link, useLocation } from "react-router-dom";
 
 const Layout = () => {
+
+  const location = useLocation();
+
   return (
     <div>
       <div className="top-bar">
         <nav className="header">
           <ul className="nav">
-            <li id="nav-item"><Link to="/">Home</Link></li>
-            <li id="nav-item"><Link to="/about">About</Link> </li>
-            <li id="nav-item"><Link to="/contact">Contact</Link></li>
+            <li id="nav-item"><Link to="/" className={location.pathname === "/" ? "active" : null}>Home</Link></li>
+            <li id="nav-item"><Link to="/about" className={location.pathname === "/about" ? "active" : null}>About</Link> </li>
+            <li id="nav-item"><Link to="/contact" className={location.pathname === "/contact" ? "active" : null}>Contact</Link></li>
 
             <img id="logo" src={require("../content/Logo.png")} alt="A logo for 24KingsUnite"/>
           </ul>
