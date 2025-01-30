91ee348b7ad7ccca10dd68934991027808e7fe5d
The24Kings <24kingsunite@gmail.com>
2025-01-30 14:28:06 -0800

    changed to have dynamic visibility

--- src/utilities/Spoiler.js
+++ src/utilities/Spoiler.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Spoiler extends Component {
     constructor(props) {
         super(props);
-        this.state = { visible: false };
+        this.state = { visible: props.visible };
     }
 
     render() {
