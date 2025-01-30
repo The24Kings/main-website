40f22874d4b236154073a3145fca1585bab4af5d
The24Kings <24kingsunite@gmail.com>
2025-01-29 19:17:12 -0800

    more blog updates

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -4,15 +4,25 @@ import ReactMarkdown from "react-markdown";
 {/* TODO: Import file from remote server of blog post and display it here */}
 
 const Blog = () => {
-    const [markdownContent, setMarkdownContent] = useState(null);
+    const [projectContent, setProjectContent] = useState("");
+    const [statusContent, setStatusContent] = useState("");
+    const [projectSpoiler, setProjectSpoiler] = useState(false);
+    const [statusSpoiler, setStatusSpoiler] = useState(false);
 
     // Fetch the blog post from the server
     useEffect(() => {
-        fetch("https://isoptera.lcsc.edu/~rjziegler/content/blog.md")
+        fetch("https://isoptera.lcsc.edu/~rjziegler/content/projects.md", { mode: "cors" })
         .then(response => response.text())
-        .then(data => { setMarkdownContent(data); })
-        .catch(e => { setMarkdownContent("Error fetching blog post..."); });
-    }, [markdownContent]);
+        .then(data => { setProjectContent(data); })
+        .catch(e => { setProjectContent("Error fetching blog post..."); });
+    }, [projectContent]);
+
+    useEffect(() => {
+        fetch("https://isoptera.lcsc.edu/~rjziegler/content/status.md", { mode: "cors" })
+        .then(response => response.text())
+        .then(data => { setStatusContent(data); })
+        .catch(e => { setStatusContent("Error fetching status post..."); });
+    }, [statusContent]);
 
     useEffect(() => {
         document.title = "Blog - Riley Ziegler";
@@ -24,11 +34,29 @@ const Blog = () => {
                 <h1 id="blog-header">What am I up to?!</h1>
 
                 <div id="blog-content">
-                    <ReactMarkdown>{markdownContent}</ReactMarkdown>
+                    <h2 id="spoiler">{projectSpoiler ? "⮟" : "⮞"} Projects</h2>
+                    <MarkdownContent content={projectContent} visible={projectSpoiler}/>
+                    <div className="toggle">
+                        <input className="button" type="button" value={projectSpoiler ? "Hide" : "Spoiler!"} onClick={() => setProjectSpoiler(!projectSpoiler)}/>
+                    </div>
+
+                    <h2 id="spoiler">{statusSpoiler ? "⮟" : "⮞"} Status Updates</h2>
+                    <MarkdownContent content={statusContent} visible={statusSpoiler}/>
+                    <div className="toggle">
+                        <input className="button" type="button" value={statusSpoiler ? "Hide" : "Spoiler!"} onClick={() => setStatusSpoiler(!statusSpoiler)}/>
+                    </div>
                 </div>
             </div>
         </React.Fragment>
     )
+};
+
+const MarkdownContent = ({content, visible}) => {
+    return visible ? (
+        <div id="content">
+            <ReactMarkdown>{content}</ReactMarkdown>
+        </div>
+    ) : null;
 };
 
 export default Blog;
