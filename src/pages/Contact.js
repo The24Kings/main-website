171aa46e20e4b6ff7cf6ea55e0c63d115230680a
The24Kings <24kingsunite@gmail.com>
2025-01-30 16:53:29 -0800

    added React.Fragment around contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,10 +6,12 @@ const Contact = () => {
     }, []);
 
     return (
-        <div className="container">
-            <h1 id="contact-header">Contact Me!</h1>
-            <p id="contact-content">Feel free to contact me at 🡆 <a id="link" href="https://gmail.com">business@24kingsunite.net</a></p>
-        </div>
+        <React.Fragment>
+            <div className="container">
+                <h1 id="contact-header">Contact Me!</h1>
+                <p id="contact-content">Feel free to contact me at 🡆 <a id="link" href="https://gmail.com">business@24kingsunite.net</a></p>
+            </div>
+        </React.Fragment>
     )
 };
 
