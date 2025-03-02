2ca3b4fc0aed9b5b3b3775f575820abdc420600a
The24Kings <24kingsunite@gmail.com>
2025-03-01 21:42:51 -0800

    hover anim stored in const

--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -4,52 +4,58 @@ import { Link, useLocation } from "react-router";
 import { motion } from 'framer-motion';
 
 const Navigation = () => {
-  const location = useLocation();
+    const location = useLocation();
 
-return (
-    <React.Fragment>
-        <div className="nav-container">
-            <div className="pfp">
-                <img id="image" src="https://isoptera.lcsc.edu/~rjziegler/pictures/Logo.jpg" onClick={() => window.open("https://github.com/The24Kings")} title="Github" alt="Logo"/>
+    const hoverNav = {
+        hover: {
+            scale: 1.05
+        }
+    };
+
+    return (
+        <React.Fragment>
+            <div className="nav-container">
+                <div className="pfp">
+                    <img id="image" src="https://isoptera.lcsc.edu/~rjziegler/pictures/Logo.jpg" onClick={() => window.open("https://github.com/The24Kings")} title="Github" alt="Logo"/>
+                </div>
+
+                <div id="info">
+                    <h1>Riley Ziegler</h1>
+                    <h5 id="github-link" title="Github" onClick={() => window.open("https://github.com/The24Kings")}>SOFTWARE DEVELOPER</h5>
+                </div>
+
+                <nav id="nav">
+                    <ul id="nav-list">
+                        <li id="nav-item">
+                            <motion.div variants={hoverNav} whileHover="hover">
+                                <Link to="/" className={location.pathname === "/" ? "active" : null}>Home</Link>
+                            </motion.div>
+                        </li>
+                        <li id="nav-item">
+                            <motion.div variants={hoverNav} whileHover="hover">
+                                <Link to="/about" className={location.pathname === "/about" ? "active" : null}>About</Link>
+                            </motion.div>
+                        </li>
+                        <li id="nav-item">
+                            <motion.div variants={hoverNav} whileHover="hover">
+                                <Link to="/projects" className={location.pathname === "/projects" ? "active" : null}>Projects</Link>
+                            </motion.div>
+                        </li>
+                        <li id="nav-item">
+                            <motion.div variants={hoverNav} whileHover="hover">
+                                <Link to="/contact" className={location.pathname === "/contact" ? "active" : null}>Contact</Link>
+                            </motion.div>
+                        </li>
+                    </ul>
+                </nav>
+
+                <footer id="social">
+                    <img id="social-image" src="https://isoptera.lcsc.edu/~rjziegler/pictures/Github.png" onClick={() => window.open("https://github.com/The24Kings")} title="Github" alt="Link to Github"/>
+                    <img id="social-image" src="https://isoptera.lcsc.edu/~rjziegler/pictures/linkedin.png" onClick={() => window.open("https://linkedin.com/in/riley-ziegler/")} title="linkedin" alt="Link to Linkedin"/>
+                </footer>
             </div>
-
-            <div id="info">
-                <h1>Riley Ziegler</h1>
-                <h5 id="github-link" title="Github" onClick={() => window.open("https://github.com/The24Kings")}>SOFTWARE DEVELOPER</h5>
-            </div>
-
-            <nav id="nav">
-                <ul id="nav-list">
-                    <li id="nav-item">
-                        <motion.div whileHover={{ scale: 1.05 }}>
-                            <Link to="/" className={location.pathname === "/" ? "active" : null}>Home</Link>
-                        </motion.div>
-                    </li>
-                    <li id="nav-item">
-                        <motion.div whileHover={{ scale: 1.05 }}>
-                            <Link to="/about" className={location.pathname === "/about" ? "active" : null}>About</Link>
-                        </motion.div>
-                    </li>
-                    <li id="nav-item">
-                        <motion.div whileHover={{ scale: 1.05 }}>
-                            <Link to="/projects" className={location.pathname === "/projects" ? "active" : null}>Projects</Link>
-                        </motion.div>
-                    </li>
-                    <li id="nav-item">
-                        <motion.div whileHover={{ scale: 1.05 }}>
-                            <Link to="/contact" className={location.pathname === "/contact" ? "active" : null}>Contact</Link>
-                        </motion.div>
-                    </li>
-                </ul>
-            </nav>
-
-            <footer id="social">
-                <img id="social-image" src="https://isoptera.lcsc.edu/~rjziegler/pictures/Github.png" onClick={() => window.open("https://github.com/The24Kings")} title="Github" alt="Link to Github"/>
-                <img id="social-image" src="https://isoptera.lcsc.edu/~rjziegler/pictures/linkedin.png" onClick={() => window.open("https://linkedin.com/in/riley-ziegler/")} title="linkedin" alt="Link to Linkedin"/>
-            </footer>
-        </div>
-    </React.Fragment>
-);
+        </React.Fragment>
+    );
 };
 
 export default Navigation;
