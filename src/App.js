586899b7a59f8851bab008bfbfa47e5d79fc78e7
The24Kings <24kingsunite@gmail.com>
2025-03-08 21:16:40 -0800

    moved navigation into flexbox hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route, HashRouter } from "react-router";
-import { AnimatePresence } from "framer-motion";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 import Navigation from "./pages/Navigation";
 import Home from "./pages/Home";
@@ -9,35 +8,41 @@ import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
-
-import transition from "./utilities/Animation";
+import { clickAnim } from "./utilities/Animation";
 
 function App() {
-  return (
-    <AnimatePresence exitBeforeEnter={true}>
-        <HashRouter>
-            <main>
-                <Navigation/>
+    const [isOpen, setIsOpen] = useState(false);
 
-                <motion.div 
-                    className="container" 
-                    variants={transition}
-                    initial="initial"
-                    animate="animate"
-                    exit="exit"
-                >
-                    <Routes>
-                        <Route index element={<Home />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/projects" element={<Projects />} />
-                        <Route path="/contact" element={<Contact />} />
-                        <Route path="/*" element={<NoPage />} />
+    const menuColor = "#2F3E46";
+
+    return (
+        <AnimatePresence exitBeforeEnter={true}>
+            <HashRouter>
+                <header>
+                    <motion.svg 
+                        className="menu-toggle" 
+                        variants={clickAnim} whileTap="click"
+                        viewBox="0 0 24 24" fill="none" 
+                        onClick={() => setIsOpen(!isOpen)}
+                    >
+                        <path d="M4 18L20 18" stroke={menuColor} strokeWidth="2" strokeLinecap="round" />
+                        <path d="M4 12L20 12" stroke={menuColor} strokeWidth="2" strokeLinecap="round" />
+                        <path d="M4 6L20 6" stroke={menuColor} strokeWidth="2" strokeLinecap="round" />
+                    </motion.svg>
+                </header>
+
+                <Navigation isOpen={isOpen} setIsOpen={setIsOpen} />
+
+                <Routes>
+                    <Route index element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/*" element={<NoPage />} />
                 </Routes>
-                </motion.div>
-            </main>
-        </HashRouter>
-    </AnimatePresence>
-  );
+            </HashRouter>
+        </AnimatePresence>
+    );
 }
 
 export default App;
--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -1,55 +1,99 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
-import { Link, useLocation } from "react-router";
+import { useLocation } from "react-router";
 import { motion } from 'framer-motion';
 
-import { hoverNav, clickAnim} from "../utilities/Animation";
+import { hoverNav, clickAnim } from "../utilities/Animation";
 
-const Navigation = () => {
+const Navigation = ({ isOpen, setIsOpen }) => {
     const location = useLocation();
+    const menuRef = useRef(null);
+
+    useEffect(() => {
+        if (menuRef.current === null) {
+            return;
+        }
+
+        if (isOpen) {
+            menuRef.current.classList.add("open");
+        } else {
+            menuRef.current.classList.remove("open");
+        }
+    }, [isOpen]);
+
+    const switchWindow = (path) => {
+        // Change the window location
+        window.location.href = "/#/" + path;
+
+        // Close the menu with a delay
+        setTimeout(() => {
+            setIsOpen(false);
+        }, 50);
+    };
 
     return (
         <React.Fragment>
-            <header className="nav-container">
-                <div className="pfp">
-                    <img id="image" src="https://isoptera.lcsc.edu/~rjziegler/pictures/Logo.jpg" onClick={() => window.open("https://github.com/The24Kings")} title="Github" alt="Logo"/>
+            {isOpen && (
+                <div className="nav-container" ref={menuRef}>
+                    <img id="image" src="https://isoptera.lcsc.edu/~rjziegler/pictures/Logo.jpg" onClick={() => window.open("https://github.com/The24Kings")} title="Github" alt="Logo" />
+
+                    <div id="info">
+                        <h1>Riley Ziegler</h1>
+                        <h4 id="github-link" title="Github" onClick={() => window.open("https://github.com/The24Kings")}>SOFTWARE DEVELOPER</h4>
+                    </div>
+                    <nav id="nav">
+                        <ul id="nav-list">
+                            <motion.li
+                                id="nav-item"
+                                className={location.pathname === "/" ? "active" : null}
+                                variants={hoverNav} whileHover="hover" whileTap="click"
+                                onClick={() => switchWindow("")}
+                            >
+                                Home
+                            </motion.li>
+                            <motion.li 
+                                id="nav-item" 
+                                className={location.pathname === "/about" ? "active" : null} 
+                                variants={hoverNav} whileHover="hover" whileTap="click" 
+                                onClick={() => switchWindow("about")}
+                            >
+                                About
+                            </motion.li>
+                            <motion.li 
+                                id="nav-item" 
+                                className={location.pathname === "/projects" ? "active" : null} 
+                                variants={hoverNav} whileHover="hover" whileTap="click" 
+                                onClick={() => switchWindow("projects")}
+                            >
+                                Projects
+                            </motion.li>
+                            <motion.li 
+                                id="nav-item" 
+                                className={location.pathname === "/contact" ? "active" : null}
+                                variants={hoverNav} whileHover="hover" whileTap="click" 
+                                onClick={() => switchWindow("contact")}
+                            >
+                                Contact
+                            </motion.li>
+                        </ul>
+                    </nav>
+
+                    <footer id="social">
+                        <motion.img id="social-image" 
+                            variants={clickAnim} whileTap="click" 
+                            src="https://isoptera.lcsc.edu/~rjziegler/pictures/Github.png" 
+                            onClick={() => window.open("https://github.com/The24Kings")} 
+                            title="Github" alt="Link to Github"
+                        />
+                        <motion.img id="social-image" 
+                            variants={clickAnim} whileTap="click" 
+                            src="https://isoptera.lcsc.edu/~rjziegler/pictures/linkedin.png" 
+                            onClick={() => window.open("https://linkedin.com/in/riley-ziegler/")} 
+                            title="linkedin" alt="Link to Linkedin" 
+                        />
+                    </footer>
                 </div>
-
-                <div id="info">
-                    <h1>Riley Ziegler</h1>
-                    <h5 id="github-link" title="Github" onClick={() => window.open("https://github.com/The24Kings")}>SOFTWARE DEVELOPER</h5>
-                </div>
-
-                <nav id="nav">
-                    <ul id="nav-list">
-                        <li id="nav-item">
-                            <motion.div variants={hoverNav} whileHover="hover" whileTap="click">
-                                <Link to="/" className={location.pathname === "/" ? "active" : null}>Home</Link>
-                            </motion.div>
-                        </li>
-                        <li id="nav-item">
-                            <motion.div variants={hoverNav} whileHover="hover" whileTap="click">
-                                <Link to="/about" className={location.pathname === "/about" ? "active" : null}>About</Link>
-                            </motion.div>
-                        </li>
-                        <li id="nav-item">
-                            <motion.div variants={hoverNav} whileHover="hover" whileTap="click">
-                                <Link to="/projects" className={location.pathname === "/projects" ? "active" : null}>Projects</Link>
-                            </motion.div>
-                        </li>
-                        <li id="nav-item">
-                            <motion.div variants={hoverNav} whileHover="hover" whileTap="click">
-                                <Link to="/contact" className={location.pathname === "/contact" ? "active" : null}>Contact</Link>
-                            </motion.div>
-                        </li>
-                    </ul>
-                </nav>
-
-                <footer id="social">
-                    <motion.img variants={clickAnim} whileTap="click" id="social-image" src="https://isoptera.lcsc.edu/~rjziegler/pictures/Github.png" onClick={() => window.open("https://github.com/The24Kings")} title="Github" alt="Link to Github"/>
-                    <motion.img variants={clickAnim} whileTap="click" id="social-image" src="https://isoptera.lcsc.edu/~rjziegler/pictures/linkedin.png" onClick={() => window.open("https://linkedin.com/in/riley-ziegler/")} title="linkedin" alt="Link to Linkedin"/>
-                </footer>
-            </header>
+            )}
         </React.Fragment>
     );
 };
